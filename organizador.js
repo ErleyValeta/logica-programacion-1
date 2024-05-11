0036fbb61493eef38f2c6b50961f7863f2a6d1b5
ErleyValeta <erley.valeta@genstudents.org>
2024-05-11 14:11:25 -0500

    Organizador

---
+++ organizador.js
@@ -0,0 +1,14 @@
+let num1 = window.prompt("ingrese 1er numero");
+let num2 = window.prompt("ingrese 2do numero");
+let num3 = window.prompt("ingrese 3er numero");
+
+
+// dorganizar numeros
+
+if (num1<num2 && num2<num3) {
+    alert ("los numeros estan organizados asi :"+ num3+"-"+num2+"-"+num1+"el otro orden es :" + mun1 +" "+ num2 +" "+ num3);
+} else{
+    if(num2<num1 && num1<num3) {
+        alert ("los numeros estan organizados asi :"+ num3+"-"+num2+"-"+num1+"el otro orden es :" + mun1 +" "+ num2 +" "+ num3); 
+    }
+};
